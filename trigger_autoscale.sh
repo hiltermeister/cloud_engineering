908144ee0bbdcb543fdbb6fc3d6bb7a28d04d589
Plosie <7846678+Plosie@users.noreply.github.com>
2022-02-19 00:28:15 +0100

    Add files via upload

---
+++ trigger_autoscale.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+URL="http://mywebloadbalancer-1736393159.us-east-1.elb.amazonaws.com:3000"
+for i in {1..1000}; do
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+  curl $URL && sleep 0.1 & 
+done
